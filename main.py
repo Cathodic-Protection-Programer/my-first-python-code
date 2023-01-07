54df933a6fed0ddb1b6a1c3b88aa2134f7563e5a
Asus-Hasan <kejoramedia@gmail.com>
2023-01-08 06:38:47 +0700

    creating the team

---
+++ main.py
@@ -0,0 +1,9 @@
+# while loop
+clean = input ("Are the hands clean now (Yes or No)? ")
+while clean == "No":
+    print("Please continue washing your hands until cleaned...")
+else:
+    print("Stop")
+
+
+
